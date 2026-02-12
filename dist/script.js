24a65ca1e0a9ed889298ca188be34c468346e16c
Sofia <itssonyamisyura@gmail.com>
2026-02-12 20:24:59 +0100

    delayed popup

--- dist/script.js
+++ dist/script.js
@@ -975,6 +975,8 @@ function () {
   _createClass(Slider, [{
     key: "showSlides",
     value: function showSlides(n) {
+      var _this = this;
+
       if (n > this.slides.length) {
         //> number of slides, return to the beginning
         this.slideIndex = 1;
@@ -983,6 +985,22 @@ function () {
       if (n < 1) {
         this.slideIndex = this.slides.length; //=last slide
       }
+
+      try {
+        this.hanson.style.opacity = '0'; //hiding
+
+        if (n === 3) {
+          // if on slide 3
+          this.hanson.classList.add('animated');
+          setTimeout(function () {
+            _this.hanson.style.opacity = '1';
+
+            _this.hanson.classList.add('slideInUp');
+          }, 3000);
+        } else {
+          this.hanson.classList.remove('slideInUp');
+        }
+      } catch (error) {}
 
       this.slides.forEach(function (slide) {
         slide.style.display = 'none'; // hide slides
@@ -998,18 +1016,22 @@ function () {
   }, {
     key: "render",
     value: function render() {
-      var _this = this;
+      var _this2 = this;
+
+      try {
+        this.hanson = document.querySelector('.hanson');
+      } catch (error) {}
 
       this.btns.forEach(function (btn) {
         btn.addEventListener('click', function () {
-          _this.plusSlides(1);
+          _this2.plusSlides(1);
         });
         btn.parentNode.previousElementSibling.addEventListener('click', function (e) {
           e.preventDefault(); // because it is a link
 
-          _this.slideIndex = 1;
+          _this2.slideIndex = 1;
 
-          _this.showSlides(_this.slideIndex);
+          _this2.showSlides(_this2.slideIndex);
         });
       });
       this.showSlides(this.slideIndex);
